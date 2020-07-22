f79129c13f45cac7f36856e4177be14183d100fd
jangnyer <jangnyer@naver.com>
2020-07-22 11:07:59 +0900

    실습용 워드 카운트

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\jangn\onedrive\바탕 화면\homework-master\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
